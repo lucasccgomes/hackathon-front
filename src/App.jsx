f4a56d6e76e6a191fc0238945f9c652ae2ca702e
Lucasccgomes <lucasccgomes@protonmail.com>
2023-12-05 16:15:36 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import Conversion from "./components/Conversion";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Conversion
+        bonds={[
+          { name: 'LFT', price: 1000, icon: '/path/to/lft/icon.svg' },
+          { name: 'LTN', price: 900, icon: '/path/to/ltn/icon.svg' },
+          { name: 'NTN-F', price: 1100, icon: '/path/to/ntnf/icon.svg' },
+          { name: 'NTN-B', price: 950, icon: '/path/to/ntnb/icon.svg' },
+          { name: 'NTN-B Principal', price: 1050, icon: '/path/to/ntnb-principal/icon.svg' },
+          { name: 'NTN-C', price: 900, icon: '/path/to/ntnc/icon.svg' },
+          // Add other bonds as needed
+        ]}
+        currencies={[
+          { name: 'BRL', rate: 5.2, icon: '/path/to/brl/icon.svg' },
+          { name: 'USD', rate: 1, icon: '/path/to/usd/icon.svg' },
+          // ... other currencies
+        ]}
+      />
+    </div>
   )
 }
