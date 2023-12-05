db5a6ac27079028af0dc6baa0331436ede7f2657
Lucasccgomes <lucasccgomes@protonmail.com>
2023-12-05 16:42:37 -0300

    update icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import Conversion from "./components/Conversion";
+import IconDocument from "./assets/document-svgrepo-com.svg";
+import IconUsd from "./assets/usd-svgrepo-com.svg";
+import IconBrl from "./assets/real.png";
 
 export default function App() {
   return (
     <div>
       <Conversion
         bonds={[
-          { name: 'LFT', price: 1000, icon: '/path/to/lft/icon.svg' },
-          { name: 'LTN', price: 900, icon: '/path/to/ltn/icon.svg' },
-          { name: 'NTN-F', price: 1100, icon: '/path/to/ntnf/icon.svg' },
-          { name: 'NTN-B', price: 950, icon: '/path/to/ntnb/icon.svg' },
-          { name: 'NTN-B Principal', price: 1050, icon: '/path/to/ntnb-principal/icon.svg' },
-          { name: 'NTN-C', price: 900, icon: '/path/to/ntnc/icon.svg' },
+          { name: 'LFT', price: 1000, icon: <IconDocument />},
+          { name: 'LTN', price: 900, icon: <IconDocument />},
+          { name: 'NTN-F', price: 1100, icon: <IconDocument />},
+          { name: 'NTN-B', price: 950, icon: <IconDocument />},
+          { name: 'NTN-B Principal', price: 1050, icon: <IconDocument />},
+          { name: 'NTN-C', price: 900, icon: <IconDocument />},
           // Add other bonds as needed
         ]}
         currencies={[
-          { name: 'BRL', rate: 5.2, icon: '/path/to/brl/icon.svg' },
-          { name: 'USD', rate: 1, icon: '/path/to/usd/icon.svg' },
+          { name: 'BRL', rate: 5.2, icon: <img src={IconBrl} alt="BRL" />},
+          { name: 'USD', rate: 1, icon: <img src={IconUsd} alt="USD" /> },
           // ... other currencies
         ]}
       />
